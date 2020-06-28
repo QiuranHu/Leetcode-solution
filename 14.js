bd419fcc708e847065922efcfdaad59d79d1887c
Qiuran Hu <qiuranh@gmail.com>
2020-06-28 09:15:10 +0800

    new file:   14.js

---
+++ 14.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    const comparePrefix = (str1, str2) => {
+        const prefixArray = [];
+        for(let i = 0; i < str1.length; i++){
+            if(str1[i] !== str2[i]) {
+                break;
+            } else {
+                prefixArray.push(str1[i]);
+            }
+        }
+        return prefixArray.join("");
+    };
+    if(strs.length === 0) {
+        return "";
+    }
+    let result = strs[0];
+    for(let i = 1; i < strs.length; i++){
+        result = comparePrefix(result, strs[i]);
+    }
+    return result;
+};
