bb88cb50ee1372ab2bff22583863155631135091
Qiuran Hu <qiuranh@gmail.com>
2020-08-04 08:47:07 +0800

    modified:   2.js

--- 2.js
+++ 2.js
@@ -11,59 +11,33 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let result = null;
-    let pointerInResult = null;
-    let pointerInL1 = l1;
-    let pointerInL2 = l2;
-    let add = 0;
-    while(pointerInL1 !== null && pointerInL2 !== null) {
-        let value = pointerInL1.val + pointerInL2.val + add;
-        if(value >= 10) {
-            add = Math.floor(value / 10);
-            value = value % 10;
-        } else {
-            add = 0;
-        }
-        pointerInL1 = pointerInL1.next;
-        pointerInL2 = pointerInL2.next;
-        if(pointerInResult === null) {
-            result = new ListNode(value, null);
-            pointerInResult = result;
-        }
-        else {
-            pointerInResult.next = new ListNode(value, null);
-            pointerInResult = pointerInResult.next;
-        }
+    const sentinal = new ListNode(0, null);
+    let current = sentinal;
+    let carry = 0;
+    while(l1 !== null && l2 !== null) {
+        const result = l1.val + l2.val + carry;
+        current.next = new ListNode(result % 10, null);
+        current = current.next;
+        carry = Math.floor(result / 10);
+        l1 = l1.next;
+        l2 = l2.next;
     }
-    let notNullPointer = null;
-    if(pointerInL1 !== null) {
-        notNullPointer = pointerInL1;
+    let list = null;
+    if(l1 !== null) {
+        list = l1;
     }
-    if(pointerInL2 !== null) {
-        notNullPointer = pointerInL2;
+    if(l2 !== null) {
+        list = l2;
     }
-    while(notNullPointer !== null) {
-        let value = notNullPointer.val + add;
-        if(value >= 10) {
-            add = Math.floor(value / 10);
-            value = value % 10;
-        } else {
-            add = 0;
-        }
-        notNullPointer = notNullPointer.next;
-        if(pointerInResult === null) {
-            result = new ListNode(value, null);
-            pointerInResult = result;
-        }
-        else {
-            pointerInResult.next = new ListNode(value, null);
-            pointerInResult = pointerInResult.next;
-        }
+    while(list !== null) {
+        const result = list.val + carry;
+        current.next = new ListNode(result % 10, null);
+        current = current.next;
+        carry = Math.floor(result / 10);
+        list = list.next;
     }
-    while(add !== 0) {
-        pointerInResult.next = new ListNode(add % 10, null);
-        pointerInResult = pointerInResult.next;
-        add = Math.floor(add / 10);
+    if(carry !== 0) {
+        current.next = new ListNode(carry, null);
     }
-    return result;
-};
+    return sentinal.next;
+}
