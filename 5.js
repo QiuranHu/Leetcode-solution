ad9f41d5544e43e498d22b4b59c83df5796866db
Qiuran Hu <qiuranh@gmail.com>
2020-08-19 22:01:32 +0800

    modified:   5.js

--- 5.js
+++ 5.js
@@ -1,5 +1,4 @@
 /**
- * Time complexity: n ^ 2
  * @param {string} s
  * @return {string}
  */
@@ -7,48 +6,38 @@ var longestPalindrome = function(s) {
     // arr[x][y] === true means the substring starting at index x 
     // and ends at index y (inclusive) is palindromic.
     arr = [];
+    let maxLength = 0;
+    let start = -1;
+    let end = -1;
     for(let i = 0; i < s.length; i ++) {
         arr.push([]);
         for(let j = 0; j < s.length; j++) {
-            arr[i].push(false);
-        }
-    }
-    let length = 0;
-    let longest = '';
-    for(let i = 0; i < s.length; i++) {
-        if(length === 0) {
-            longest = s.substr(0, 1);
-            length = longest.length;
-        }
-        arr[i][i] = true;
-        if(i + 1 < s.length) {
-            if(s[i] === s[i + 1]) {
-                const j = i + 1;
-                arr[i][j] = true;
-                if(j - i + 1 > length) {
-                    length = j - i + 1;
-                    longest = s.substr(i, length);
-                }
-            }
+            arr[i].push(null);
         }
     }
     
-    for(let j = 2; j < s.length; j ++) {
+    // Go up.
+    for(let j = 0; j < s.length; j++) {
         for(let i = 0; i < s.length; i++) {
-            // calculate arr[i][j]
-            if(i + j < s.length) {
-                if(s[i] === s[i + j]) {
-                    if(arr[i + 1][i + j - 1]) {
-                        arr[i][i + j] = true;
-                        if(j + 1 > length) {
-                            length = j + 1;
-                            longest = s.substr(i, length);
-                        }
-                    }
-                }
+            if(i + j >= s.length) {
+                continue;
+            }
+            if(j === 0) {// First compute the diagonal, the go up.
+                arr[i][i] = true;
+            } else if(j === 1) {// Go up one level;
+                arr[i][i + 1] = s[i] === s[i + 1] ? true : false; 
+            } else if(s[i] === s[i + j] && arr[i + 1][i + j - 1] === true) {
+                arr[i][i + j] = true;
+            } else {
+                arr[i][i + j] = false;
+            }
+            if(arr[i][i + j] === true && j + 1 > maxLength) {
+                maxLength = j + 1;
+                start = i;
+                end = i + j;
             }
         }
     }
-    return longest;
+    return s.substr(start, end - start + 1);
     
 };
